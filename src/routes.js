8af3415b0a6e1fec2891b8e60b9a5fca6e061546
Isaias Mendes <isaiasmendes@gmail.com>
2019-04-12 04:47:16 -0300

    add ap backend

---
+++ src/routes.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const multer = require('multer')
+const multerConfig = require('./config/multer')
+
+const routes = express.Router()
+
+// add controllers
+const BoxController = require('./controllers/BoxController')
+const FileController = require('./controllers/FileController')
+
+routes.get('/', (req, res) => {
+   return res.send('Olá, Isaías')
+})
+
+routes.get('/boxes/:id', BoxController.show)
+
+routes.post('/boxes', BoxController.store)
+
+routes.post('/boxes/:id/files', 
+   multer(multerConfig).single('file'), 
+   FileController.store
+)
+
+module.exports = routes
---
+++ src/server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+io.on("connection", socket => {
+   socket.on('connectRoom', box => {
+      socket.join(box)
+   })
+})
+
+//add conection mongoose
+mongoose.connect(
+   'mongodb+srv://userNode:userNode123@cluster0-4bkoq.mongodb.net/dbTest?retryWrites=true', 
+   {
+      useNewUrlParser: true
+   }
+)
+ 
+// add Middleware
+app.use((req, res, next) => {
+   req.io = io
+   return next()
+})
+
+app.use(express.json()) 
+app.use(express.urlencoded({ extended: true }))
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')))
+
+// add routes
+app.use(require('./routes'))
+
+server.listen(3300) 
