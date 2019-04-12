f4ca75d57aba5d4a4529599e882a1d403c3d736e
Isaias Mendes <isaiasmendes@gmail.com>
2019-04-12 20:53:20 -0300

    alteraoes endpoint 2

--- src/routes.js
+++ src/routes.js
@@ -25,7 +25,7 @@ routes.post('/boxes/:id/files',
       const valor = req.params.val
       const name = req.params.name
       const real = valor == 1 ? "real" : "reais"
-      return res.json(`Obrigado, ${name}, voce fez uma doacao de R$ ${valor} ${real}!`)
+      return res.json(`Obrigado, ${name}! Voce fez uma doacao de R$ ${valor} ${real}!`)
    })
 
 module.exports = routes
